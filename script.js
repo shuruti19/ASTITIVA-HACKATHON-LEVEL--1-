4ea1a46d64bd01dccc566b3f6eb2a362ef2f1d6e
shuruti19 <shurutiraj19@gmail.com>
2024-09-05 13:52:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const hamburger=document.querySelector('.hamburger-menu');
+const navMenu=document.querySelector('.nav-menu');
+hamburger.addEventListener('click',()=>{
+    navMenu.classList.toggle('hide');
+});
